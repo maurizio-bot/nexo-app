000400770c96057c940615e097436321e911ae30
MaurizioAmi <84752171+maurizio-bot@users.noreply.github.com>
2026-03-15 09:48:34 -0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,17 @@ module.exports = {
     filename: 'js/[name].[contenthash:8].js',
     chunkFilename: 'js/[name].[contenthash:8].chunk.js',
     clean: true,
-    publicPath: '/'
+    publicPath: './'  // Cambiado de '/' a './' para compatibilidad con Capacitor/Android
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,  // Nueva regla para SASS/SCSS
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -36,13 +40,16 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './Index.html',
+      template: './index.html',  // Cambiado: ahora apunta a index.html (minúscula) en raíz
       filename: 'index.html',
       inject: 'body',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
-        removeRedundantAttributes: true
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
       }
     }),
     new CopyWebpackPlugin({
@@ -51,17 +58,31 @@ module.exports = {
           from: 'src/assets',
           to: 'assets',
           noErrorOnMissing: true
+        },
+        {
+          from: 'client/assets',  // Fallback: assets legacy si aún no migraste todo
+          to: 'assets',
+          noErrorOnMissing: true,
+          globOptions: {
+            ignore: ['**/.*']  // Ignorar archivos ocultos
+          }
+        },
+        {
+          from: 'client/android',  // Si tienes recursos android específicos en client/
+          to: 'android',
+          noErrorOnMissing: true
         }
       ]
     })
   ],
   resolve: {
-    extensions: ['.js', '.json', '.css'],
+    extensions: ['.js', '.json', '.css', '.scss'],  // Agregado .scss
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@core': path.resolve(__dirname, 'src/core'),
       '@vault': path.resolve(__dirname, 'src/vault'),
-      '@stream': path.resolve(__dirname, 'src/stream')
+      '@stream': path.resolve(__dirname, 'src/stream'),
+      '@styles': path.resolve(__dirname, 'src/styles')  // Nuevo alias útil
     }
   },
   optimization: {
@@ -72,8 +93,19 @@ module.exports = {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
           chunks: 'all'
+        },
+        styles: {  // Nuevo: separa CSS en chunks optimizados
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true
         }
       }
     }
+  },
+  performance: {
+    hints: false,  // Desactiva warnings de archivos grandes en desarrollo
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   }
 };
