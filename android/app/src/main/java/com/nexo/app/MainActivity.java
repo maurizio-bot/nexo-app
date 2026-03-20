c3498780bbd7337d60d56fd82e8dbcf138f80a8e
MaurizioAmi <84752171+maurizio-bot@users.noreply.github.com>
2026-03-20 01:44:57 -0600

    Update MainActivity.java

--- android/app/src/main/java/com/nexo/app/MainActivity.java
+++ android/app/src/main/java/com/nexo/app/MainActivity.java
@@ -1,5 +1,15 @@
 package com.nexo.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.nexo.ble.NexoBlePlugin;  // Import del plugin
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    
+    // REGISTRO CRÍTICO: Plugin BLE local
+    registerPlugin(NexoBlePlugin.class);
+  }
+}
