aaaa38f6ac74e91cbf6458abebe19f803f78aee1
MaurizioAmi <84752171+maurizio-bot@users.noreply.github.com>
2026-05-05 09:17:06 -0600

    Register NexoNearbyPlugin in MainActivity

--- android/app/src/main/java/com/nexo/app/MainActivity.java
+++ android/app/src/main/java/com/nexo/app/MainActivity.java
@@ -2,14 +2,14 @@ package com.nexo.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.nexo.ble.NexoBlePlugin;  // Import del plugin
+import com.nexo.ble.NexoBlePlugin;
+import com.nexo.ble.NexoNearbyPlugin;
 
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
-    // REGISTRO CRÍTICO: Plugin BLE local
     registerPlugin(NexoBlePlugin.class);
+    registerPlugin(NexoNearbyPlugin.class);
   }
 }
